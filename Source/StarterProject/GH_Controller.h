a191e56bf8efd8c4ca8eb3937d40d3d8955abebe
Fran <frna.vlz@gmail.com>
2021-02-09 23:19:53 -0700

    collision

---
+++ Source/StarterProject/GH_Controller.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GH_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARTERPROJECT_API UGH_Controller : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
